a5e6b1b9393eb9e9f32776e638ab6027dadf1365
FlowerDeLuce <zly4e4ka666@rambler.ru>
2017-05-04 21:52:55 +0300

    landing with slick

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+	browserSync = require('browser-sync');
+
+	gulp.task('sass', function(){
+		return gulp.src('app/sass/main.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+	});
+	
+	
+	
+	gulp.task('browser-sync', function(){
+		browserSync({
+			server: {
+				baseDir: 'app'
+			}
+		});
+		
+	});
+	
+	gulp.task('watch',['browser-sync', 'sass'], function(){
+		gulp.watch('app/sass/main.sass', ['sass']);
+		gulp.watch('app/*.html', browserSync.reload);
+	});
