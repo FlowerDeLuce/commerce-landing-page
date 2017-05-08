ec1d04e07843e0af7db515dbe2bfc7814c7d393e
FlowerDeLuce <zly4e4ka666@rambler.ru>
2017-05-08 23:44:31 +0300

    responsive version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$('.js-price__slider').slick({
+		nextArrow: '.pack-slider__next',
+		prevArrow: '.pack-slider__prev',
 dots: true,
   infinite: false,
   speed: 300,
@@ -7,7 +9,7 @@ dots: true,
   slidesToScroll: 3,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 840,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
@@ -16,14 +18,7 @@ dots: true,
       }
     },
     {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
+      breakpoint: 640,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
@@ -38,5 +33,45 @@ dots: true,
 		nextArrow: '.reviev-slider__next',
 		prevArrow: '.reviev-slider__prev'
 	});
+	$('.main-menu--mobile').click( function(){
+		$(this).toggleClass('active');
+		$('.main-menu__list').toggleClass('active');
+	});
 	
+	$('.js-about__slider').slick({
+dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+   
+    {
+      breakpoint: 860,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+	});
+	
+	$("#request-form").submit(function(){
+     $("#request-form").css("display","none");
+	 $("#request-form--success").css("display","block");
+	 return false;
+});
+	$("#region-form").submit(function(){
+		 $("#region-form").css("display","none");
+		 $("#region-form--success").css("display","block");
+		  return false;
+	});
 });
